3a203a12d7cef4b7db33169367ed7ad9982cddfc
kouser-ahamed <ahamed15-5643@diu.edu.bd>
2026-04-30 16:09:06 +0600

    fix(signup): remove Google sign-in button and related functionality from sign-up page

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -3,7 +3,6 @@
 import { authClient } from "@/lib/auth-client";
 import { Button, Card, Form, Input } from "@heroui/react";
 import { useRouter } from "next/navigation";
-import { GrGoogle } from "react-icons/gr";
 
 export default function SignUpPage() {
   const router = useRouter();
@@ -34,14 +33,6 @@ export default function SignUpPage() {
       router.push("/");
     }
   };
-
-  const handleGoogleSignIn = async () => {
-
-    await authClient.signIn.social({
-        provider: "google",
-    })
-  };
-
 
   return (
     <div className="flex justify-center mt-16 px-4">
@@ -91,20 +82,6 @@ export default function SignUpPage() {
             Sign Up
           </Button>
         </Form>
-
-         <p className="text-center">Or</p>
-        
-                <Button
-                  onClick={handleGoogleSignIn}
-                  className="w-full flex items-center justify-center gap-2 
-                  bg-white text-gray-700 
-                  border border-gray-200 
-                  hover:bg-gray-50 
-                  transition-all duration-200"
-                >
-                  <GrGoogle className="text-[#EA4335] text-lg" />
-                  Sign in with Google
-                </Button>
 
         <p className="text-center text-sm text-gray-500 mt-4">
           Already have an account?{" "}
